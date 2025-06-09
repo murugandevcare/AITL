e6f17630161dcf3b7759e04e95a60c5bc7bbedc6
Murugan <murugan.m@devcare.com>
2025-06-09 20:29:47 +0530

    Changes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import "./global.css"
+import "./global.css";
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Login from './src/features/auth/Login';
+import SignIn from './src/features/auth/SignIn';
+
+
+const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
   return (
-    <View className="flex-1 justify-center items-center gap-5 bg-blue-500">
-      <Text className="text-4xl  text-white font-tamilRegular" style={{ fontFamily: 'NotoSansTamil_Condensed-Bold' }}>வணக்கம்!</Text>
-      <Text className="text-4xl  text-white font-tamilBold">வணக்கம்!</Text>
-      <Text className="text-4xl text-white">வணக்கம்!</Text>
-       <Text style={{ fontFamily: 'Roboto', fontSize: 22 }}>
-        Hello, Roboto Regular!
-      </Text>
-      <Text style={{ fontFamily: 'Roboto-Bold', fontSize: 22 }}>
-        Hello, Roboto Bold!
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignIn">
+        <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}} />
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ src/features/auth/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Login: React.FC = () => {
+  return (
+    <View className="flex-1 justify-center items-center bg-green-300">
+      <Text className="text-lg font-bold text-red-500">Wellcome</Text>
+    </View>
+  );
+};
+
+export default Login;
